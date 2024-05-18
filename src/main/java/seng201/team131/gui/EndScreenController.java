02a36d962afe9ee14a2d46376569cd721a6ea7ad
Benjamin <bsk29@uclive.ac.nz>
2024-05-18 15:31:39 +1200

    Wokring code

--- src/main/java/seng201/team131/gui/EndScreenController.java
+++ src/main/java/seng201/team131/gui/EndScreenController.java
@@ -3,7 +3,7 @@ package seng201.team131.gui;
 import seng201.team131.Player;
 
 public class EndScreenController extends Controller{
-    private Player;
+    private Player player;
     @Override
     public void setPlayer(Player player) {
         this.player = player;
