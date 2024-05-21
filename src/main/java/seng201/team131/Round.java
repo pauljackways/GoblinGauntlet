64f1dcb91aecb2e3cc6752c5cc2f22c71cb72708
Paul Jackways <pja117@uclive.ac.nz>
2024-05-21 21:30:47 +1200

    ready for carts

--- src/main/java/seng201/team131/Round.java
+++ src/main/java/seng201/team131/Round.java
@@ -1,76 +1,43 @@
 package seng201.team131;
 
-//import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
-import static seng201.team131.EnumResources.*;
-
 public class Round {
     private List<Tower> towerList;
     private Integer cartCount;
     private Float moneyThisRound;
-    private List<Cart> carts;
-    private Integer cartCapacity;
+    private Integer roundNumber;
+    private Float cartCapacity;
     private Integer speed; // milliseconds to travel 10m
-    private Integer favourResource;
-    private Integer currentRound;
-    private Integer round;
     private Double moneyPerCart;
-    private Integer distance;
-    private Boolean SABOTAGE;
-    private Player player;
-
-
     private Random random; 
 
-    public Round(Integer round) {
-        this.favourResource = player.getFavorResource();
-        carts = new ArrayList<>();
-        favourResource = 2;
+    public Round(Integer roundNumber) {
         random = new Random();
-
-    }
-    public Round() {
 
     }
   
     public void applyDifficulty(EnumGamechangers difficulty) {
-
-        //int roundNumber = player.getRound();
         switch (difficulty) {
             case EASY:
-                this.cartCount = 5;
-                cartCapacity = 5;
+                this.cartCount = 5 * roundNumber;
+                cartCapacity = 5.0f;
                 speed = 1200;
 
                 break;
 
             case MEDIUM:
-                this.cartCount = 10;
-                cartCapacity = 10;
+                this.cartCount = 10 * roundNumber;
+                cartCapacity = 10.0f;
                 speed = 1000;
                 break;
-
             case HARD:
-                this.cartCount = 15;
-                cartCapacity = 15;
+                this.cartCount = 15 * roundNumber;
+                cartCapacity = 15.0f;
                 speed = 800;
                 break;
             default:
                 break;
-        }
-
-        for (int i = 0; i < this.cartCount; i++) {
-            Cart newCart = new Cart();
-            carts.add(newCart);
-            newCart.setCapacity(cartCapacity);
-            newCart.setFillLevel(0);
-            int randomResouce = random.nextInt(3);
-            if(randomResouce == 0){newCart.setResourceType(LAVA);}
-            if(randomResouce == 1){newCart.setResourceType(ETHER);}
-            if(randomResouce == 2){newCart.setResourceType(GOO);}
         }
     }
 
@@ -108,38 +75,6 @@ public class Round {
                     moneyPerCart = moneyPerCart * 1.2;
                 }
                 break;
-
-            case DISTRIBUTION:
-                if (favourResource == 0) {
-                    chanceETHER = 0.6;
-                    chanceLava = 0.2;
-                    chanceGoo = 0.2;
-                }
-                if (favourResource == 1) {
-                    chanceETHER = 0.2;
-                    chanceLava = 0.6;
-                    chanceGoo = 0.2;
-                }
-                if (favourResource == 2) {
-                    chanceETHER = 0.2;
-                    chanceLava = 0.2;
-                    chanceGoo = 0.6;
-                }
-                if (favourResource == null) {
-                    chanceETHER = 0.3333;
-                    chanceLava = 0.3333;
-                    chanceGoo = 0.3333;
-                }
-
-
-                if (randomValue < chanceETHER) {
-                    //newCart.setResourceType(ETHER);
-                } else if (randomValue < chanceETHER + chanceLava) {
-                    //newCart.setResourceType(LAVA);
-                } else {
-                    //newCart.setResourceType(GOO);
-                }
-                break;
             default:
                 break;
         }
@@ -147,13 +82,16 @@ public class Round {
     public Integer getSpeed() {
         return speed;
     }
+    public Float getCartCapacity() {
+        return cartCapacity;
+    }
+    public Integer getCartCount() {
+        return cartCount;
+    }
     public void setTowers(List<Tower> towerList) {
         this.towerList = towerList;
     }
     public List<Tower> getTowers() {
         return towerList;
-    }
-    public List<Cart> getCarts() {
-        return carts;
     }
 }
