23d031e1d207900059ae3c4a955fc8952cc520a7
Benjamin <bsk29@uclive.ac.nz>
2024-05-22 02:37:55 +1200

    Added side bar functionality to the gamechnagers screen

--- src/main/java/seng201/team131/Cart.java
+++ src/main/java/seng201/team131/Cart.java
@@ -54,6 +54,10 @@ public class Cart implements Runnable {
         return resourceType;
     }
 
+    public Integer getFavourResource(){
+        return favourResource;
+    }
+
     public Float getFillLevel() {
         return fillLevel;
     }
--- src/main/java/seng201/team131/Gamechangers.java
+++ src/main/java/seng201/team131/Gamechangers.java
@@ -4,17 +4,77 @@ public class Gamechangers implements Selectable {
     private String name;
     private String image;
     private String description;
-    public Gamechangers() {
+    private String resourceMoreCommon;
 
+    public Gamechangers() {
     }
-    public Gamechangers(EnumGamechangers type) {
+    public Gamechangers(EnumGamechangers type,  Integer resourceMoreCommon) {
+        switch(resourceMoreCommon){
+            case 0 -> this.resourceMoreCommon = "Ether";
+            case 1 -> this.resourceMoreCommon = "Lava";
+            case 2 -> this.resourceMoreCommon = "Goo";
+            default -> this.resourceMoreCommon = "Unknown";
+        }
+
+
         switch(type) {
             case EASY -> {
-                this.description = "Order orcs to build you another\nlevel on one of your towers.\n\nFill more carts at greater volumes";
-                this.image = "images/upgrade.jpeg";
+                this.description = "You could probaby do Medium";
+                this.image = "images/baby.jpeg";
+                break;
+            }
+            case MEDIUM -> {
+                this.description = "You could probaby go to hard, \nhow hard could it be?";
+                this.image = "images/medium.jpeg";
 
                 break;
             }
+            case HARD -> {
+                this.description = "Are You sure?";
+                this.image = "images/big.jpeg";
+
+                break;
+            }
+            case FAST -> {
+                this.description = "Speeds up the carts, \nbut you make more money. \nGo on take the Chance";
+                this.image = "images/speed.jpeg";
+                break;
+            }
+            case SABOTAGE -> {
+                this.description = "Makes carts of a random \nresource fill up slower, \nbut you can make more money.";
+                this.image = "images/sabotage.jpeg";
+
+                break;
+            }
+            case THEFT -> {
+                this.description = "Carts of a random resource fill \nup slower";
+                this.image = "/images/OIG4..jpeg";
+
+                break;
+            }
+            case SLOW -> {
+                this.description = "Pretty self explanatory really...";
+                this.image = "images/slow.jpeg";
+                break;
+            }
+            case MONEY -> {
+                this.description = "20% money boost for \none round";
+                this.image = "images/rich.jpeg";
+
+                break;
+            }
+            case DISTRIBUTION -> {
+                this.description = "Makes " + this.resourceMoreCommon + " carts more likey";
+                this.image = "images/distribution.jpeg";
+
+                break;
+            }
+            default -> {
+                break;
+            }
+        
+            
+
             
 
         }
@@ -24,9 +84,9 @@ public class Gamechangers implements Selectable {
         return this.name;
     }
     public String getImage() {
-        return "";
+        return this.image;
     }
     public String getDescription() {
-        return "";
+        return this.description;
     }
 }
